7e3b716f9f8afa81c36868fea927657c418c09f2
SENECHAL Louis <130775686+LudwigvonSchlett@users.noreply.github.com>
2024-05-31 15:53:35 +0200

    error_404

--- sonic/epresence/views.py
+++ sonic/epresence/views.py
@@ -2,16 +2,16 @@ from django.shortcuts import render
 
 
 def error_400(request, exception):
-    return render(request, 'main/400.html', status=400)
+    return render(request, 'epresence/400.html', status=400)
 
 def error_403(request, exception):
-    return render(request, 'main/403.html', status=403)
+    return render(request, 'epresence/403.html', status=403)
 
 def error_404(request, exception):
-    return render(request, 'main/404.html', status=404)
+    return render(request, 'epresence/404.html', status=404)
     
 def error_405(request, exception):
-    return render(request, 'main/405.html', status=405)    
+    return render(request, 'epresence/405.html', status=405)    
 
 def error_500(request):
-    return render(request, 'main/500.html', status=500)
+    return render(request, 'epresence/500.html', status=500)
