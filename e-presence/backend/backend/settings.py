153e208d3ccf1235f5c6c4f841a21ef80446dbe1
LinkCheat <153827536+LinkCheat@users.noreply.github.com>
2024-06-04 16:27:49 +0200

    baseline

--- e-presence/backend/backend/settings.py
+++ e-presence/backend/backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'corsheaders',
     'rest_framework',
     'api',
 ]
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = "backend.urls"
@@ -71,6 +73,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "backend.wsgi.application"
 
+CORS_ORIGIN_ALLOW_ALL = True  # Pour permettre toutes les origines (à configurer de manière plus sécurisée en production)
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
--- e-presence/backend/backend/urls.py
+++ e-presence/backend/backend/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import login_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("login/", login_view, name='login'),
 ]
