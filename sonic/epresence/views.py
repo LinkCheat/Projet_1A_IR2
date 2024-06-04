b06e368af999283ca0ad0815880d329cc039ee36
Usf <you.rochdi02@gmail.com>
2024-06-04 10:53:19 +0200

    Ajout du nv rep 1.0

--- sonic/epresence/views.py
+++ sonic/epresence/views.py
@@ -1,4 +1,34 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+from django.contrib.auth.models import User
+
+@csrf_exempt
+def login_view(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        email = data.get('email')
+        password = data.get('password')
+        try:
+            user = User.objects.get(email=email)
+            username = user.username
+        except User.DoesNotExist:
+            return JsonResponse({'message': 'Adresse e-mail ou mot de passe incorrect'}, status=400)
+        
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return JsonResponse({'message': 'Connexion réussie'})
+        else:
+            return JsonResponse({'message': 'Adresse e-mail ou mot de passe incorrect'}, status=400)
+    return JsonResponse({'message': 'Requête invalide'}, status=400)
+
+
+def index(request):
+    return render(request, 'index.html')
+
 
 
 def error_400(request, exception):
