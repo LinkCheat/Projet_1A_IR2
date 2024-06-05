d9073c615d5046277e7250ab68b52629fee23ed9
salmalam-ai <salmalam2004@gmail.com>
2024-06-06 00:08:09 +0200

    ajout de la page d'acceuil pour les profs

---
+++ epresence_frontend/src/acceuiprof.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.getElementById('menu-toggle');
+    const sidebar = document.getElementById('sidebar');
+    const mainContent = document.getElementById('main-content');
+
+    menuToggle.addEventListener('click', function() {
+        sidebar.classList.toggle('expanded');
+        if (sidebar.classList.contains('expanded')) {
+            mainContent.style.marginLeft = '250px';
+        } else {
+            mainContent.style.marginLeft = '0';
+        }
+    });
+});
+
+
+    window.changeContent = function(section) {
+        
+        const titleMap = {
+            'evaluation': 'Évaluation',
+            'absences': 'Absences',
+            'parametres': 'Paramètres'
+        };
+        const contentMap = {
+            'evaluation': 'Manage evaluations here.',
+            'absences': 'Manage absences here.',
+            'parametres': 'Adjust settings here.'
+        };
+
+        
+        mainContent.innerHTML = `<h2>${titleMap[section]}</h2><p>${contentMap[section]}</p>`;
+    };
+
