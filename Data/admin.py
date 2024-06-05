739561101b0b2a3fd6f9f585ffb0e61a1c13e32a
Alban283746 <154105366+Alban283746@users.noreply.github.com>
2024-06-05 10:47:40 -0400

    data

---
+++ Data/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Absences,Filieres,Matieresir,Tableir,Utilisateurs
+
+
+admin.site.register([Absences,Filieres,Matieresir,Tableir,Utilisateurs])
+
