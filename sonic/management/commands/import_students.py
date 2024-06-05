6852fd77d3861e2376049bc647292ec1222487d1
Clovis Tiemtore <clovistiemtore402@gmail.com>
2024-06-05 15:14:30 +0200

    implement students, subjects

--- sonic/management/commands/import_students.py
+++ sonic/management/commands/import_students.py
@@ -1,23 +1,23 @@
 from django.core.management.base import BaseCommand
 from epresence_api.models import Student
-import os
-import csv
+import csv, os
 
 class Command(BaseCommand):
     help = 'Import student data from a CSV file'
 
-    def handle(self, *args, **options):
-        # Assuming the CSV file is in the same directory as this script
-        current_dir = os.path.dirname(os.path.abspath(__file__))
-        csv_file_path = os.path.join(current_dir, 'students.csv')
+    def add_arguments(self, parser):
+        parser.add_argument('file_path', type=str, help='The path to the CSV file containing the student data')
 
-        if not os.path.exists(csv_file_path):
-            self.stdout.write(self.style.ERROR(f"File not found: {csv_file_path}"))
+    def handle(self, *args, **options):
+        file_path = options['file_path']
+
+        if not os.path.exists(file_path):
+            self.stdout.write(self.style.ERROR(f"File not found: {file_path}"))
             return
 
         students_to_import = []
 
-        with open(csv_file_path, 'r') as file:
+        with open(file_path, 'r') as file:
             reader = csv.reader(file)
             next(reader)  # Skip the header row
             for row in reader:
