2d0e67e1dabf3e6054b45193b93c2b9c9dee0ba5
Usf <you.rochdi02@gmail.com>
2024-06-06 13:54:37 +0200

    Mise à jour des paramètres Django

--- sonic/sonic/settings.py
+++ sonic/sonic/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from decouple import config
 
+SECRET_KEY = config('SECRET_KEY')
+
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(' ')])
+
+
+
 from pathlib import Path
 import os
 
@@ -78,6 +84,8 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'sonic.wsgi.application'
+
+ALLOWED_HOSTS=[]
 
 
 # Database
