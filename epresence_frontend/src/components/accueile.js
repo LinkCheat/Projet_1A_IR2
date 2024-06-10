e185d75fc26b6d14d40cd0e3fd0e16644712bf33
SENECHAL Louis <130775686+LudwigvonSchlett@users.noreply.github.com>
2024-06-10 19:11:18 +0200

    Update accueile.js

--- epresence_frontend/src/components/accueile.js
+++ epresence_frontend/src/components/accueile.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import styles from '../assets/styles/acceuile.module.css';
+import styles from '../assets/styles/accueile.module.css';
 
 const AccueilEtudiant = () => {
   return (
